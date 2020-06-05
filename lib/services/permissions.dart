721fee838815b31d4d73f971fa76cddd7cb48114
Luis Correa <correaleyval@gmail.com>
2020-06-04 22:21:25 -0400

    permissions handler

---
+++ lib/services/permissions.dart
@@ -0,0 +1,29 @@
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionsService {
+  final PermissionHandler _permissionHandler = PermissionHandler();
+
+  Future<bool> requestPermission(PermissionGroup permission) async {
+    var result = await _permissionHandler.requestPermissions([permission]);
+    if (result[permission] == PermissionStatus.granted) {
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<bool> hasPermission(PermissionGroup permission) async {
+    var permissionStatus =
+        await _permissionHandler.checkPermissionStatus(permission);
+    return permissionStatus == PermissionStatus.granted;
+  }
+
+  Future<bool> requestPhonePermission() async {
+    return requestPermission(PermissionGroup.phone);
+  }
+
+  Future<bool> hasPhonePermission() async {
+    return hasPermission(PermissionGroup.phone);
+  }
+}
+
--- lib/services/phone.dart
+++ lib/services/phone.dart
@@ -1,7 +1,13 @@
 import 'package:intent/intent.dart' as android_intent;
 import 'package:intent/action.dart' as android_action;
 
+import 'package:todo/services/permissions.dart';
+
 void callTo(String number) async {
+  if(! await PermissionsService().hasPhonePermission())
+    if (! await PermissionsService().requestPhonePermission())
+      return;
+
   android_intent.Intent()
     ..setAction(android_action.Action.ACTION_CALL)
     ..setData(Uri(scheme: "tel", path: number))
