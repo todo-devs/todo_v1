ab98227d143e161757908a515651a5f54c28b232
Luis Correa <correaleyval@gmail.com>
2020-06-22 18:53:46 -0400

    dark mode ok

---
+++ lib/themes/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GFColors {
+  static const Color PRIMARY = Color(0xff3880FF);
+  static const Color SECONDARY = Color(0xffAA66CC);
+  static const Color SUCCESS = Color(0xff10DC60);
+  static const Color INFO = Color(0xff33B5E5);
+  static const Color WARNING = Color(0xffFFBB33);
+  static const Color DANGER = Color(0xffF04141);
+  static const Color LIGHT = Color(0xffE0E0E0);
+  static const Color DARK = Color(0xff222428);
+  static const Color WHITE = Color(0xffffffff);
+  static const Color FOCUS = Color(0xff434054);
+  static const Color ALT = Color(0xff794c8a);
+  static const Color TRANSPARENT = Colors.transparent;
+}
