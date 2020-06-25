ec6ead668f556a46736e72c97ab025269c8943fd
Roig <groig@protonmail.com>
2020-06-24 20:39:13 -0400

    fix typos

--- lib/components/disclaim.dart
+++ lib/components/disclaim.dart
@@ -75,7 +75,7 @@ https://github.com/todo-devs/
 
 con el objetivo de facilitar el acceso a los servicios de ETECSA.
 
-Se recomienda al usuario descargar siempre la aplicación desde las fuentes oficiales, las cuáles se listan al final de este escrito y que esté al tanto de las actualizaciones para que la aplicación funcione correctamente. 
+Se recomienda al usuario descargar siempre la aplicación desde las fuentes oficiales, las cuales se listan al final de este escrito y que esté al tanto de las actualizaciones para que la aplicación funcione correctamente. 
 
 No se almacena ni se exporta ningún tipo de información personal del usuario ni del uso que hace de la aplicación.
 
@@ -83,7 +83,7 @@ Los servicios solicitados mediante la aplicación responden a las prestaciones d
 
 El código de la aplicación se encuentra disponible bajo la licencia GPL3, cualquier tipo de copia, modificación y compilación del código deben hacerse respetando la licencia y bajo principios éticos, no se aceptarán contribuciones al código que traten de engañar a los usuarios o de exportar información de los mismos a servidores externos.
 
-UNA VEZ MÁS. HACEMOS ÉNFASIS EN QUE LOS USUARIOS DEBEN DESCARGAR LA APLICACIÓN DESDE FUENTES OFICIALES:
+UNA VEZ MÁS HACEMOS ÉNFASIS EN QUE LOS USUARIOS DEBEN DESCARGAR LA APLICACIÓN DESDE FUENTES OFICIALES:
 
 FUENTES OFICIALES:
 
