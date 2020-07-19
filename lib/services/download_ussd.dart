b5917a7d3cdf399d2bafcd418ab5ff0ba419b221
Carlos Torres <karlosgm75@gmail.com>
2020-07-18 20:09:12 -0400

    Added support for requests using http proxies

--- lib/services/download_ussd.dart
+++ lib/services/download_ussd.dart
@@ -1,45 +1,65 @@
 import 'dart:convert';
+import 'dart:developer';
 
+import 'package:device_proxy/device_proxy.dart';
+import 'package:dio/adapter.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class DownloadUssdService {
   Future<String> fetchHash() async {
-    final resp = await http.get(
-      'https://todo-devs.github.io/todo-json/hash.json',
-      headers: {
-        'Accept-Encoding': 'gzip, deflate, br',
-      },
-    );
-    if (resp.statusCode == 200) {
-      var json = jsonDecode(resp.body);
-      var hash = json['hash'];
-      return hash;
+    var dio = await _getDio();
+    var response = await dio
+        .get('https://todo-devs.github.io/todo-json/hash.json',
+        options: Options(headers: {'Accept-Encoding': 'gzip, deflate, br'}));
+
+    if (response.statusCode == 200) {
+      
+      var body = response.data as Map<String, dynamic>;
+
+      return json.encode(body);
     } else {
       throw Exception(
-        'Request failed: ${resp.request.url}\n'
-        'StatusCode: ${resp.statusCode}\n'
-        'Body: ${resp.body}',
+        'Request failed: ${response.request.uri}\n'
+            'StatusCode: ${response.statusCode}\n'
+            'Body: ${response.data}',
+      );
+    }
+
+  }
+
+  Future<String> fetchUssdConfig() async {
+    var dio = await _getDio();
+    log('test');
+    var response = await dio
+        .get('https://todo-devs.github.io/todo-json/config.json',
+        options: Options(headers: {'Accept-Encoding': 'gzip, deflate, br'}));
+
+    if (response.statusCode == 200) {
+      var body = response.data as Map<String, dynamic>;
+
+      return json.encode(body);
+    } else {
+      throw Exception(
+        'Request failed: ${response.request.uri}\n'
+        'StatusCode: ${response.statusCode}\n'
+        'Body: ${response.data}',
       );
     }
   }
 
-  Future<String> fetchUssdConfig() async {
-    final resp = await http.get(
-      'https://todo-devs.github.io/todo-json/config.json',
-      headers: {
-        'Accept-Encoding': 'gzip, deflate, br',
-      },
-    );
-    if (resp.statusCode == 200) {
-      var body = utf8.decode(resp.bodyBytes);
-
-      return body;
-    } else {
-      throw Exception(
-        'Request failed: ${resp.request.url}\n'
-        'StatusCode: ${resp.statusCode}\n'
-        'Body: ${resp.body}',
-      );
+  Future<Dio> _getDio() async {
+    ProxyConfig proxyConfig = await DeviceProxy.proxyConfig;
+    var dio = Dio();
+    if (proxyConfig.isEnable) {
+      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+          (client) {
+        client.findProxy = (uri) {
+          return "PROXY ${proxyConfig.proxyUrl};";
+        };
+        client.connectionTimeout = Duration(seconds: 10);
+      };
     }
+    return dio;
   }
 }
