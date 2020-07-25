e3bf23a194168db1f4caf7c3d8f5701eba6d0c6f
Luis Correa <correaleyval@gmail.com>
2020-07-25 00:53:33 -0400

    More actions to home widget

--- android/app/src/main/kotlin/com/cubanopensource/todo/HomeWidgetProvider.kt
+++ android/app/src/main/kotlin/com/cubanopensource/todo/HomeWidgetProvider.kt
@@ -3,9 +3,11 @@ package com.cubanopensource.todo
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.provider.Settings
 import android.widget.RemoteViews
 
 class HomeWidgetProvider : AppWidgetProvider() {
@@ -30,6 +32,17 @@ class HomeWidgetProvider : AppWidgetProvider() {
             val pendingIntentTODO = PendingIntent.getActivity(context, 0,
                     Intent(context, MainActivity::class.java), 0)
 
+            val pendingIntentWifi = PendingIntent.getActivity(context, 0, Intent(Settings.ACTION_WIFI_SETTINGS), 0)
+            val pendingIntentAPN = PendingIntent.getActivity(context, 0, Intent(Settings.ACTION_APN_SETTINGS), 0)
+
+            val dataUseIntent = Intent(Intent.ACTION_MAIN)
+            dataUseIntent.component = (ComponentName(
+                    "com.android.settings",
+                    "com.android.settings.Settings\$DataUsageSummaryActivity"
+            ))
+            dataUseIntent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK and Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+            val pendingIntentData = PendingIntent.getActivity(context, 0, dataUseIntent, 0)
 
             val views = RemoteViews(context?.packageName, R.layout.home_widget)
 
@@ -39,6 +52,9 @@ class HomeWidgetProvider : AppWidgetProvider() {
             views.setOnClickPendingIntent(R.id.widget_button_saldo_voz, pendingIntentVoz)
             views.setOnClickPendingIntent(R.id.widget_button_saldo_sms, pendingIntentSms)
             views.setOnClickPendingIntent(R.id.todo_logo, pendingIntentTODO)
+            views.setOnClickPendingIntent(R.id.widget_button_net_wifi, pendingIntentWifi)
+            views.setOnClickPendingIntent(R.id.widget_button_net_apn, pendingIntentAPN)
+            views.setOnClickPendingIntent(R.id.widget_button_net_datos, pendingIntentData)
 
             appWidgetManager?.updateAppWidget(it, views)
         }
